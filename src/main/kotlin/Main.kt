9062acafa13b09197fe789efd1e0cd0657929d85
julierealmids <jullietnakayizaa@gmail.com>
2022-03-02 17:41:51 +0300

    operators assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var name = arrayOf("julliet nakayiza","clare migisha","getrude namusoke")
+    println(name[0])
+    println(name[1])
+    println(name[2])
+
+    var age = arrayOf<Int>(22,25,24)
+    println(age)
+    var num = arrayOf(+25678034598375,+256394759048,+256438920468)
+    println(num[0])
+    println(num[1])
+    println(num[2])
+    var weight =arrayOf("56","58","79")
+    println(weight[0])
+    println(weight[1])
+    println(weight[2])
+    var notkenyan:Boolean = true
+    println(notkenyan)
+}
